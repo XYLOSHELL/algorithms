777f8446441f9b5cc560efe1d632c575bcb6ac48
Danila Chuklanov <tschuklanow@yandex.ru>
2019-02-24 17:44:16 +0300

    +OK code style

--- bubble.py
+++ bubble.py
@@ -6,5 +6,9 @@ def bubble_sort(array):
                 array[j], array[j + 1] = array[j + 1], array[j]
     return array
 
-array = [int(s) for s in input().split()]
-print(bubble_sort(array))
+def main():
+    array = [int(s) for s in input().split()]
+    print(bubble_sort(array))
+
+if __name__ == '__main__':
+    main()
