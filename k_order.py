2efd1400a923e8aa0dba9612c472e928fcb71664
Danila Chuklanov <tschuklanow@yandex.ru>
2019-02-24 17:03:13 +0300

    Corrections:    -bugs    +OK code style

--- k_order.py
+++ k_order.py
@@ -1,18 +1,20 @@
-from random import *
+from random import randint
 
 def k_order_stat(array, k):
-    if len(array) == 1:
+    if len(set(array)) == 1:
         return array[0]
     x = array[randint(0, len(array) - 1)]
     left = [y for y in array if y < x]
     right = [y for y in array if y >= x]
     if len(left) >= k:
         return k_order_stat(left, k)
-    elif len(left) == 0:
-        return array[0]
     else:
         return k_order_stat(right, k - len(left))
 
-k = int(input())
-array = [int(s) for s in input().split()]
-print(k_order_stat(array, k))
+def main():
+    k = int(input())
+    array = [int(s) for s in input().split()]
+    print(k_order_stat(array, k))
+
+if __name__ == '__main__':
+    main()
