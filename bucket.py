fdfb5d7e67ad3041b4298bd8d0b33395e603dd49
Danila Chuklanov <tschuklanow@yandex.ru>
2019-02-24 17:38:32 +0300

    +OK code style

--- bucket.py
+++ bucket.py
@@ -1,27 +1,22 @@
-from math import *
-
 def bucket_sort(array, bucket_size):
-	if len(array) == 1:
-		return array
-	min_val = array[0]
-	max_val = array[0]
-	for i in range(0, len(array)):
-		if array[i] < min_val:
-			min_val = array[i]
-		if array[i] > max_val:
-			max_val = array[i]
-	num_bucket = floor((max_val - min_val) / bucket_size) + 1
-	buckets = []
-	for i in range(0, num_bucket):
-		buckets.append([])
-	for i in range(0, len(array)):
-		buckets[floor((array[i] - min_val) / bucket_size)].append(array[i])
-	sort_arr = []
-	for i in range(0, len(buckets)):
-		buckets[i].sort()
-		for j in range(0, len(buckets[i])):
-			sort_arr.append(buckets[i][j])
-	return sort_arr
+    if len(array) == 1:
+        return array
+    min_val = min(array)
+    max_val = max(array)
+    num_bucket = (max_val - min_val) // bucket_size + 1
+    buckets = [[] for _ in range(num_bucket)]
+    for x in array:
+        buckets[(x - min_val) // bucket_size].append(x)
+    sort_arr = []
+    for bucket in buckets:
+        bucket.sort()
+        for x in bucket:
+            sort_arr.append(x)
+    return sort_arr
 
-array = [int(s) for s in input().split()]
-print(bucket_sort(array, len(array)))
+def main():
+    array = [int(s) for s in input().split()]
+    print(bucket_sort(array, len(array)))
+
+if __name__ == '__main__':
+    main()
